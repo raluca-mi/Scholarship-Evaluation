6a896d7f0213f46f276cfca354246ca13ac909b1
raluca-mi <raluca.ioana169@gmail.com>
2022-03-28 15:20:36 +0300

    Added new module and model class

---
+++ interns-app/src/app/base/base.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    //components to export to app.module
+  ]
+})
+export class BaseModule { }
